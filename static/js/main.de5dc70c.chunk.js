8994bfb991974b88bf475225359f99fa0025a501
Jake <jacob.lindsey88@gmail.com>
2021-09-28 01:33:54 -0500

    Updates

--- static/js/main.de5dc70c.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpmemory-game"]=this["webpackJsonpmemory-game"]||[]).push([[0],{11:function(e,n,t){"use strict";t.r(n);var c=t(0),r=t.n(c),o=t(3),i=t.n(o),s=(t(8),t(9),t(1));var a=function(){return Object(s.jsx)("div",{children:"TEST"})};i.a.render(Object(s.jsx)(r.a.StrictMode,{children:Object(s.jsx)(a,{})}),document.getElementById("root"))},8:function(e,n,t){},9:function(e,n,t){}},[[11,1,2]]]);
-//# sourceMappingURL=main.de5dc70c.chunk.js.map
